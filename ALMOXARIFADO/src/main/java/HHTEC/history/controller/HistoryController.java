32373233950ce0772f74a798e21b2a0eece9f4d9
luanFidelis <gustavolu4n@gmail.com>
2026-04-14 19:47:30 -0300

    Update HistoryController.java

--- ALMOXARIFADO/src/main/java/HHTEC/history/controller/HistoryController.java
+++ ALMOXARIFADO/src/main/java/HHTEC/history/controller/HistoryController.java
@@ -21,50 +21,71 @@ import HHTEC.history.service.HistoryService;
 @RestController
 @RequestMapping("/v2/History")
 public class HistoryController {
-   
+    
     private static HistoryService historyService;
+    
     public HistoryController(HistoryService historyService){
-
         this.historyService = historyService;
     }
 
+    // http://localhost:8080/v2/History/VerLista
+    // TIPO: GET
+    // BODY: Não precisa de body
     @GetMapping("/VerLista")
     public List<History> seeList (){
-     
         return historyService.listHistory();
-        
     }
 
+    // http://localhost:8080/v2/History/buscarId/1   <-- Coloque o ID na URL
+    // TIPO: GET
+    // BODY: Não precisa de body
     @GetMapping("/buscarId/{id}")
     public ResponseEntity<?> verId (@PathVariable Long id) {
-
         Optional<History> result = historyService.SeeID(id);
-
         return result
         .map(ResponseEntity::ok)
         .orElse(ResponseEntity.notFound().build());
     }
     
+    // http://localhost:8080/v2/History/add
+    // TIPO: POST
+    // BODY:
+    // {
+    //   "nameProduct": "Nome do Material",
+    //   "colaboratorName": "Nome do Colaborador (ou null)",
+    //   "state": "STATUS_DO_MATERIAL"
+    // }
     @PostMapping("/add")
     public ResponseEntity<History> entryHistory(@RequestBody HistoryDto historyDto) {
-        
-        History history = new History(historyDto.getNameProduct(), historyDto.getColaboratorName(), historyDto.getState());
+        History history = new History(
+            historyDto.getNameProduct(), 
+            historyDto.getColaboratorName(), 
+            historyDto.getState()
+        );
 
         History retorno = historyService.addHistory(history);
-
         return ResponseEntity.status(201).body(retorno);
     }
     
+    // http://localhost:8080/v2/History/editStatus
+    // TIPO: POST
+    // BODY:
+    // {
+    //   "id": 1,
+    //   "colaboratorName": "Novo Colaborador (ou null)",
+    //   "state": "NOVO_STATUS"
+    // }
     @CrossOrigin(origins = "*")
     @PostMapping("/editStatus")
     public ResponseEntity<History> editStatus(@RequestBody UpdateDto update) {
-    
-        Optional<History> result = historyService.mudarStatus(update.getId(),update.getColaborattorName(), update.getState());
+        Optional<History> result = historyService.mudarStatus(
+            update.getId(),
+            update.getColaboratorName(), 
+            update.getState()
+        );
         
         return result
         .map(ResponseEntity::ok)
         .orElse(ResponseEntity.notFound().build());
     }
-   
-    
 }
